984c060cc4b54e94c3b80dcfdeefd63420d33764
Rmaroun <rmaroun@outlook.com>
2023-04-20 13:49:59 +0200

    adding marked markdown library and enter key to chat

--- packages/editor-ui/src/components/CaasView/CaasLandingPage.vue
+++ packages/editor-ui/src/components/CaasView/CaasLandingPage.vue
@@ -20,8 +20,7 @@
 				</div>-->
 			</div>
 		</div>
-		<ChatList>
-		</ChatList>
+		<ChatList></ChatList>
 	</div>
 </template>
 
@@ -32,24 +31,20 @@ import { mapStores } from 'pinia';
 import { setPageTitle } from '@/utils';
 import axios from 'axios';
 import ChatList from './components/ChatList.vue';
-// import Heads from './components/Heads.vue';
 
 export default Vue.extend({
 	name: 'caas-landing-page',
 	data() {
 		return {
-      messages: [],
-      currentMessage: null
-    }
+			messages: [],
+			currentMessage: null,
+		};
 	},
-	components: {
-    // Heads,
-    ChatList
-	},
+	components: { ChatList },
 	computed: {
 		...mapStores(useUIStore),
 		pageTitle() {
-			return 'Yuze AI - Connectors as a Service'
+			return 'Yuze AI - Connectors as a Service';
 		},
 	},
 	mounted() {
@@ -60,14 +55,11 @@ export default Vue.extend({
 			this.uiStore.addFirstStepOnLoad = true;
 		},
 		async sendMessage(message: String | null): Promise<any> {
-			this.messages.push({
-					from: 'user',
-					data: message
-			})
-			await axios.post('/api/yuzeai', { message }).then( (response) => {
-					this.messages.push(response.data)
-			})
-		}
+			this.messages.push({ from: 'user', data: message });
+			await axios.post('/api/yuzeai', { message }).then((response) => {
+				this.messages.push(response.data);
+			});
+		},
 	},
 });
 </script>
@@ -121,111 +113,100 @@ export default Vue.extend({
 }*/
 
 input {
-  width: 300px;
-  padding: 10px;
+	width: 300px;
+	padding: 10px;
 }
 button {
-  height: 40px;
-  background-color: powderblue;
-  padding: 10px;
+	height: 40px;
+	background-color: powderblue;
+	padding: 10px;
 }
 .messageBox {
-  height: 500px;
-  background-color: gainsboro;
-  /* margin-left: 10%;
+	height: 500px;
+	background-color: gainsboro;
+	/* margin-left: 10%;
   margin-right: 10%; */
-  margin: 0 20% 0 20%;
-  margin-top: 20px;
-  padding: 5%;
+	margin: 0 20% 0 20%;
+	margin-top: 20px;
+	padding: 5%;
 }
 .messageFromUser {
-  text-align: right;
-  background-color: aliceblue;
-  border-radius: 10px;
-  padding: 10px;
-  margin-top: 15px;
-  margin-bottom: 15px;
-  width: 30%;
-  margin-left: 70%;
+	text-align: right;
+	background-color: aliceblue;
+	border-radius: 10px;
+	padding: 10px;
+	margin-top: 15px;
+	margin-bottom: 15px;
+	width: 30%;
+	margin-left: 70%;
 }
 .messageFromChatGpt {
-  text-align: left;
-  background-color: antiquewhite;
-  border-radius: 10px;
-  padding: 10px;
-  margin-top: 15px;
-  margin-bottom: 15px;
-  width: 30%;
-  margin-right: 70%;
+	text-align: left;
+	background-color: antiquewhite;
+	border-radius: 10px;
+	padding: 10px;
+	margin-top: 15px;
+	margin-bottom: 15px;
+	width: 30%;
+	margin-right: 70%;
 }
 
-	/*body {
-		margin: 0px;
-		padding: 0px;
-		font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-		overflow: hidden;
-	}*/
+blockquote,
+body,
+button,
+dd,
+div,
+dl,
+dt,
+form,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+input,
+li,
+ol,
+p,
+pre,
+td,
+textarea,
+th,
+ul {
+	margin: 0;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+	padding: 0;
+	-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
 
-  blockquote,
-  body,
-  button,
-  dd,
-  div,
-  dl,
-  dt,
-  form,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  input,
-  li,
-  ol,
-  p,
-  pre,
-  td,
-  textarea,
-  th,
-  ul {
-    margin: 0;
-    margin-top: 0px;
-    margin-right: 0px;
-    margin-bottom: 0px;
-    margin-left: 0px;
-    padding: 0;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  }
+ul,
+li {
+	list-style: none;
+}
 
-  ul,
-  li {
-    list-style: none;
-  }
+a {
+	color: #666;
+}
 
-  a {
-    color: #666;
-  }
-  /*定义滚动条高宽及背景 高宽分别对应横竖滚动条的尺寸*/
+::-webkit-scrollbar {
+	width: 10px;
+	height: 10px;
+	background-color: '#F8F8F8';
+}
 
-  ::-webkit-scrollbar {
-    width: 10px;
-    height: 10px;
-    background-color: #F8F8F8;
-  }
-  /*定义滚动条轨道 内阴影+圆角*/
+::-webkit-scrollbar-track {
+	background-color: '#F8F8F8';
+}
 
-  ::-webkit-scrollbar-track {
-    background-color: #F8F8F8;
-  }
-  /*定义滑块 内阴影+圆角*/
+::-webkit-scrollbar-thumb {
+	background-color: '#e2e2e2';
+}
 
-  ::-webkit-scrollbar-thumb {
-    background-color: #e2e2e2;
-  }
-
-	#chatapp {
-    overflow: hidden;
-  }
-
+#chatapp {
+	overflow: hidden;
+}
 </style>
--- packages/editor-ui/src/components/CaasView/components/ChatList.vue
+++ packages/editor-ui/src/components/CaasView/components/ChatList.vue
@@ -1,442 +1,399 @@
 <template>
 	<div>
-			<section class="chatlist" :class="showSelBox>0?'chatlist-bottom-collapse':'chatlist-bottom'">
-					<mt-loadmore :top-method="loadTop" top-pull-text="Load more" top-drop-text="Release load" @top-status-change="handleTopChange" ref="loadmore">
-							<ul>
-									<template v-for="item in records">
-											<li class="chat-mine" v-if="item.type==1">
-													<div class="chat-user"><img src="../assets/user.png"></div>
-													<div class="time"><cite><i>{{item.time}}</i>{{item.name}}</cite></div>
-													<div class="chat-text" v-html="item.content"></div>
-											</li>
-											<li v-if="item.type==2">
-													<div class="chat-user"><img src="../assets/default.png"></div>
-													<div class="time"><cite>{{item.name}}<i>{{item.time}}</i></cite></div>
-													<div class="chat-text" v-html="item.content"></div>
-													<!--<div v-html="markdownToHtml(item.content)"></div>-->
-											</li>
-									</template>
-							</ul>
-					</mt-loadmore>
-			</section>
+		<section class="chatlist" :class="showSelBox > 0 ? 'chatlist-bottom-collapse' : 'chatlist-bottom'">
+			<mt-loadmore :top-method="loadTop" top-pull-text="Load more" top-drop-text="Release load"
+				@top-status-change="handleTopChange" ref="loadmore">
+				<ul>
+					<template v-for="item in records">
+						<li class="chat-mine" v-if="item.type == 1">
+							<div class="chat-user"><img src="../assets/user.png"></div>
+							<div class="time"><cite><i>{{ item.time }}</i>{{ item.name }}</cite></div>
+							<div class="chat-text" v-html="item.content"></div>
+						</li>
+						<li v-if="item.type == 2">
+							<div class="chat-user"><img src="../assets/default.png"></div>
+							<div class="time"><cite>{{ item.name }}<i>{{ item.time }}</i></cite></div>
+							<div class="chat-text" v-html="markdownToHtml(item.content)"></div>
+						</li>
+					</template>
+				</ul>
+			</mt-loadmore>
+		</section>
 
-			<section class="foot">
-					<input id="txtContent" placeholder="Please describe your connector flow" class="con" v-model="content" />
-					<span class="btn btn-send" v-on:click="sendMsg">Send</span>
-			</section>
+		<section class="foot">
+			<input
+				id="txtContent"
+				v-on:keyup.enter="sendMsg"
+				placeholder="Please describe your connector flow"
+				class="con"
+				v-model="content"
+			/>
+			<span class="btn btn-send" v-on:click="sendMsg">Send</span>
+		</section>
 	</div>
 </template>
 
 <script>
 import util from '../common/util';
+import { marked } from 'marked';
 
 export default {
 	name: 'chatlist',
 	data() {
-			return {
-					showSelBox: 0, // 0隐藏 1显示表情 2显示其他
-					selFace: 'Expression',
-					selOther: 'Other functions',
-					content:'',
-					topStatus: '',
-					//聊天记录
-					records: [{
-							type: 2,
-							time: util.formatDate.format(new Date(),'yyyy-MM-dd hh:mm:ss'),
-							name: 'YuzeAI',
-							content: 'Hello please describe your Yuzedata connector flow！'
-					}],
-					// 表情
-					EXPS: []
-			}
+		return {
+			showSelBox: 0, // 0隐藏 1显示表情 2显示其他
+			selFace: 'Expression',
+			selOther: 'Other functions',
+			content: '',
+			topStatus: '',
+			//聊天记录
+			records: [
+				{
+					type: 2,
+					time: util.formatDate.format(new Date(), 'yyyy-MM-dd hh:mm:ss'),
+					name: 'YuzeAI',
+					content: 'Hello please describe your Yuzedata connector flow！',
+				},
+			],
+			// 表情
+			EXPS: [],
+		};
 	},
 	methods: {
-			markdownToHtml(description) {
-				return description;// marked(description);
-			},
-			getEXP: function (pageNow,pageSize) {
-					return this.EXPS.slice((pageNow - 1) * pageSize, pageSize * pageNow)
-			},
-			//发送消息
-			sendMsg: function(){
-					var _this=this;
+		markdownToHtml(description) {
+			return marked(description);
+		},
+		getEXP(pageNow, pageSize) {
+			return this.EXPS.slice((pageNow - 1) * pageSize, pageSize * pageNow);
+		},
+		//发送消息
+		sendMsg() {
+			var _this = this;
 
-					if(this.content==''){
-							alert('Please describe your connector flow');
-							return;
-					}
-
-					this.records.push({
-							type: 1,
-							time: util.formatDate.format(new Date(),'yyyy-MM-dd hh:mm:ss'),
-							name: 'Tourists',
-							content: this.content
-					});
-
-					setTimeout(function(){
-							_this.records.push({
-									type: 2,
-									time: util.formatDate.format(new Date(),'yyyy-MM-dd hh:mm:ss'),
-									name: 'customer service MM',
-									content: `Here's the n8n JSON code for the flow you described:\`\`\`json
-									{
-										"nodes": [
-											{
-												"parameters": {
-													"interval": 604800000,
-													"timezone": "America/Los_Angeles"
-												},
-												"name": "Weekly Trigger",
-												"type": "n8n-nodes-base.timeTrigger",
-												"typeVersion": 1,
-												"position": [250, 300]
-											},
-											{
-												"parameters": {
-													"additionalFields": { "maxResults": 10, "query": "openai" }
-												},
-												"name": "Get Tweets",
-												"type": "n8n-nodes-base.twitter",
-												"typeVersion": 1,
-												"position": [450, 300]
-											},
-											{
-												"parameters": {
-													"functionCode": "const summary = items[0].json.data.map(tweet => tweet.text).join('\\');\return [{json: {summary}}];"
-												},
-												"name": "Summarize Tweets",
-												"type": "n8n-nodes-base.function",
-												"typeVersion": 1,
-												"position": [650, 300]
-											},
-											{
-												"parameters": {
-													"toEmails": "info@toto.com",
-													"subject": "Weekly OpenAI Tweets Summary",
-													"text": "Here's a summary of the top 10 tweets about OpenAI from the past week:\\{{$json.summary}}"
-												},
-												"name": "Send Email",
-												"type": "n8n-nodes-base.emailSend",
-												"typeVersion": 1,
-												"position": [850, 300]
-											}
-										],
-										"connections": {
-											"Weekly Trigger": {
-												"main": [[{ "node": "Get Tweets", "type": "main", "index": 0 }]]
-											},
-											"Get Tweets": {
-												"main": [[{ "node": "Summarize Tweets", "type": "main", "index": 0 }]]
-											},
-											"Summarize Tweets": {
-												"main": [[{ "node": "Send Email", "type": "main", "index": 0 }]]
-											}
-										}
-									}
-									\`\`\`
-									You will need to add your Twitter API credentials to the \`Get Tweets\` node and your email credentials to the \`Send Email\` node before running this flow.
-								`});
-					},100);
-
-					this.content='';
-
-					this.scrollToBottom();
-
-			},
-			focusTxtContent:function(){
-					document.querySelector("#txtContent input").focus();
-			},
-			scrollToBottom:function(){
-					setTimeout(function(){
-							var chatlist = document.getElementsByClassName('chatlist')[0];
-							chatlist.scrollTop=chatlist.scrollHeight;
-					},100);
-			},
-			replaceFace:function(con){
-					var _this=this;
-					var exps=this.EXPS;
-					for(var i=0;i<exps.length;i++){
-							//con = con.replace(new RegExp(exps[i].code,'g'), '<img src="static/emotion/' + exps[i].file +'"  alt="" />');
-							con = con.replace(exps[i].reg, '<img src="static/emotion/' + exps[i].file +'"  alt="" />');
-					}
-					return con;
-			},
-			handleTopChange(status) {
-					this.topStatus = status;
-			},
-			loadTop(id) {
-					var _this=this;
-					setTimeout(() => {
-							var chatlist = document.getElementsByClassName('chatlist')[0];
-							var oldHeight=chatlist.scrollHeight;
-
-							_this.records.unshift({
-									type: 1,
-									time: util.formatDate.format(new Date(),'yyyy-MM-dd hh:mm:ss'),
-									name: 'Tourists',
-									content: 'Hello! 13213'
-							}, {
-									type: 2,
-									time: util.formatDate.format(new Date(),'yyyy-MM-dd hh:mm:ss'),
-									name: 'Customer MM',
-									content: 'Here is <a target="_blank" href="https://github.com/taylorchen709/vue-chat">source code</a>13213'
-							});
-
-							setTimeout(function(){
-									var newHeight=chatlist.scrollHeight;
-									chatlist.scrollTop=newHeight-oldHeight;
-							},100);
-
-							this.$refs.loadmore.onTopLoaded(id);
-					}, 1500);
+			if (this.content === '') {
+				alert('Please describe your connector flow');
+				return;
 			}
-	},
-	mounted:function(){
+
+			this.records.push({
+				type: 1,
+				time: util.formatDate.format(new Date(), 'yyyy-MM-dd hh:mm:ss'),
+				name: 'Tourists',
+				content: this.content,
+			});
+
+			setTimeout(function () {
+				_this.records.push({
+					type: 2,
+					time: util.formatDate.format(new Date(), 'yyyy-MM-dd hh:mm:ss'),
+					name: 'customer service MM',
+					// eslint-disable-next-line n8n-local-rules/no-unneeded-backticks
+					content: `Here is the n8n workflow JSON code for the flow you described:\n\n\`\`\`\n{\n  \"nodes\": [\n    {\n      \"parameters\": {\n        \"maxResults\": 10,\n        \"query\": \"openai\"\n      },\n      \"name\": \"Twitter Search\",\n      \"type\": \"n8n-nodes-base.twitterSearch\",\n      \"id\": \"6d2b2061-5e5c-4d0e-a9c7-8fc6f8c6d2b6\",\n      \"typeVersion\": 1,\n      \"position\": [\n        250,\n        300\n      ]\n    },\n    {\n      \"parameters\": {\n        \"functionCode\": \"let tweets = items[0].json.statuses;\\nlet summary = '';\\n\\nfor (let tweet of tweets) {\\n  summary += '\\\\n' + tweet.text;\\n}\\n\\nreturn [{json: {summary: summary}}];\"\n      },\n      \"name\": \"Summarize Tweets\",\n      \"type\": \"n8n-nodes-base.function\",\n      \"typeVersion\": 1,\n      \"position\": [\n        550,\n        300\n      ],\n      \"id\": \"9b1f7f4c-4d23-4c1d-8d96-4e4f9f7d1c9b\"\n    },\n    {\n      \"parameters\": {\n        \"toEmails\": [\n          \"info@toto.com\"\n        ],\n        \"subject\": \"Weekly Summary of OpenAI Tweets\",\n        \"text\": \"Here is a summary of the latest tweets about OpenAI: {{$json['summary']}}\"\n      },\n      \"name\": \"Send Email\",\n      \"type\": \"n8n-nodes-base.emailSend\",\n      \"typeVersion\": 1,\n      \"position\": [\n        850,\n        300\n      ],\n      \"id\": \"b6f53c6f-4a4f-4e17-8c4e-0f58cc1c1d22\"\n    }\n  ],\n  \"connections\": {\n    \"Twitter Search\": {\n      \"main\": [\n        [\n          {\n            \"node\": \"Summarize Tweets\",\n            \"type\": \"main\",\n            \"index\": 0\n          }\n        ]\n      ]\n    },\n    \"Summarize Tweets\": {\n      \"main\": [\n        [\n          {\n            \"node\": \"Send Email\",\n            \"type\": \"main\",\n            \"index\": 0\n          }\n        ]\n      ]\n    }\n  }\n}\n\`\`\`\n\nThis workflow retrieves the latest 10 tweets about \"openai\" using the Twitter Search node, summarizes their content using a Function node, and then sends a weekly summary email to info@toto.com using the Email Send node.`,
+				});
+			}, 100);
+
+			this.content = '';
+
 			this.scrollToBottom();
-			this.focusTxtContent();
-	}
+		},
+		focusTxtContent() {
+			document.querySelector('#txtContent input').focus();
+		},
+		scrollToBottom() {
+			setTimeout(function () {
+				var chatlist = document.getElementsByClassName('chatlist')[0];
+				chatlist.scrollTop = chatlist.scrollHeight;
+			}, 100);
+		},
+		handleTopChange(status) {
+			this.topStatus = status;
+		},
+		loadTop(id) {
+			var _this = this;
+			setTimeout(() => {
+				var chatlist = document.getElementsByClassName('chatlist')[0];
+				var oldHeight = chatlist.scrollHeight;
+
+				_this.records.unshift(
+					{
+						type: 1,
+						time: util.formatDate.format(new Date(), 'yyyy-MM-dd hh:mm:ss'),
+						name: 'Tourists',
+						content: 'Hello! 13213',
+					},
+					{
+						type: 2,
+						time: util.formatDate.format(new Date(), 'yyyy-MM-dd hh:mm:ss'),
+						name: 'Customer MM',
+						content:
+							'Here is <a target="_blank" href="https://github.com/taylorchen709/vue-chat">source code</a>13213',
+					},
+				);
+
+				setTimeout(function () {
+					var newHeight = chatlist.scrollHeight;
+					chatlist.scrollTop = newHeight - oldHeight;
+				}, 100);
+
+				this.$refs.loadmore.onTopLoaded(id);
+			}, 1500);
+		},
+	},
+	mounted() {
+		this.scrollToBottom();
+		this.focusTxtContent();
+	},
 	// updated:function(){
 	//     this.scrollToBottom();
 	// }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-	.chatlist {
-			position: absolute;
-			top: 60px;
-			bottom: 48px;
-			left: 200px;
-			right: 0px;
-			overflow-y: scroll;
-			overflow-x: hidden;
-			padding: 10px;
-	}
+.chatlist {
+	position: absolute;
+	top: 60px;
+	bottom: 48px;
+	left: 200px;
+	right: 0px;
+	overflow-y: scroll;
+	overflow-x: hidden;
+	padding: 10px;
+}
 
-	.chatlist-bottom {
-			bottom: 48px;
-	}
+.chatlist-bottom {
+	bottom: 48px;
+}
 
-	.chatlist-bottom-collapse {
-			bottom: 198px;
-	}
+.chatlist-bottom-collapse {
+	bottom: 198px;
+}
 
-	.chatlist ul {
-			min-height: 300px;
-	}
+.chatlist ul {
+	min-height: 300px;
+}
 
-	.chatlist ul .chat-mine {
-			text-align: right;
-			padding-left: 0;
-			padding-right: 60px;
-	}
+.chatlist ul .chat-mine {
+	text-align: right;
+	padding-left: 0;
+	padding-right: 60px;
+}
 
-	.chatlist ul li {
-			position: relative;
-			/*font-size: 0;*/
-			margin-bottom: 10px;
-			padding-left: 60px;
-			min-height: 68px;
-	}
+.chatlist ul li {
+	position: relative;
+	/*font-size: 0;*/
+	margin-bottom: 10px;
+	padding-left: 60px;
+	min-height: 68px;
+}
 
-	.chat-mine .chat-user {
-			left: auto;
-			right: 3px;
-	}
+.chat-mine .chat-user {
+	left: auto;
+	right: 3px;
+}
 
-	.chat-user {
-			position: absolute;
-			left: 3px;
-	}
+.chat-user {
+	position: absolute;
+	left: 3px;
+}
 
-	.chat-text,
-	.chat-user {
-			display: inline-block;
-			vertical-align: top;
-			/*font-size: 14px;*/
-	}
+.chat-text {
+	max-width: 100%;
+}
 
-	.chat-user img {
-			width: 40px;
-			height: 40px;
-			border-radius: 100%;
-	}
+pre {
+	white-space: pre-wrap !important;
+	word-wrap: break-word !important;
+}
 
-	.time {
-			width: 100%;
-	}
+.chat-text,
+.chat-user {
+	display: inline-block;
+	vertical-align: top;
+	/*font-size: 14px;*/
+}
 
-	cite {
-			left: auto;
-			right: 60px;
-			text-align: right;
-	}
+.chat-user img {
+	width: 40px;
+	height: 40px;
+	border-radius: 100%;
+}
 
-	cite {
-			font-style: normal;
-			line-height: 24px;
-			font-size: 12px;
-			white-space: nowrap;
-			color: #999;
-			text-align: left;
-	}
+.time {
+	width: 100%;
+}
 
-	cite i {
-			font-style: normal;
-			padding-left: 5px;
-			padding-right: 5px;
-			font-size: 12px;
-	}
+cite {
+	left: auto;
+	right: 60px;
+	text-align: right;
+}
 
-	.chat-mine .chat-text {
-			margin-left: 0;
-			text-align: left;
-			background-color: #33DF83;
-			color: #fff;
-	}
+cite {
+	font-style: normal;
+	line-height: 24px;
+	font-size: 12px;
+	white-space: nowrap;
+	color: #999;
+	text-align: left;
+}
 
-	.chat-text {
-			position: relative;
-			line-height: 22px;
-			/*margin-top: 25px;*/
-			padding: 10px 15px;
-			background-color: #eee;
-			border-radius: 3px;
-			color: #333;
-			word-break: break-all;
-			max-width: 462px\9;
-	}
+cite i {
+	font-style: normal;
+	padding-left: 5px;
+	padding-right: 5px;
+	font-size: 12px;
+}
 
-	.chat-text,
-	.chat-user {
-			display: inline-block;
-			vertical-align: top;
-			font-size: 14px;
-	}
+.chat-mine .chat-text {
+	margin-left: 0;
+	text-align: left;
+	background-color: #33df83;
+	color: #fff;
+}
 
-	.chat-text img {
-			max-width: 100%;
-			vertical-align: middle;
-	}
+.chat-text {
+	position: relative;
+	line-height: 22px;
+	/*margin-top: 25px;*/
+	padding: 10px 15px;
+	background-color: #eee;
+	border-radius: 3px;
+	color: #333;
+	word-break: break-all;
+	max-width: 462px\9;
+}
 
-	.chat-user {
-			position: absolute;
-			left: 3px;
-	}
+.chat-text,
+.chat-user {
+	display: inline-block;
+	vertical-align: top;
+	font-size: 14px;
+}
 
-	.chat-text:after {
-			content: '';
-			position: absolute;
-			left: -10px;
-			top: 15px;
-			width: 0;
-			height: 0;
-			border-style: solid dashed dashed;
-			border-color: #eee transparent transparent;
-			overflow: hidden;
-			border-width: 10px;
-	}
+.chat-text img {
+	max-width: 100%;
+	vertical-align: middle;
+}
 
-	.chat-mine .chat-text:after {
-			left: auto;
-			right: -10px;
-			border-top-color: #33DF83;
-	}
+.chat-user {
+	position: absolute;
+	left: 3px;
+}
 
-	.foot {
-			width: 80%;
-			min-height: 48px;
-			position: fixed;
-			bottom: 0px;
-			left: 200px;
-			background-color: #F8F8F8;
-	}
+.chat-text:after {
+	content: '';
+	position: absolute;
+	left: -10px;
+	top: 15px;
+	width: 0;
+	height: 0;
+	border-style: solid dashed dashed;
+	border-color: #eee transparent transparent;
+	overflow: hidden;
+	border-width: 10px;
+}
 
-	.foot .con {
-		position: absolute;
-		left: 20px;
-		line-height: 30px;
-		top: 5px;
-		right: 80px;
-	}
+.chat-mine .chat-text:after {
+	left: auto;
+	right: -10px;
+	border-top-color: #33df83;
+}
 
-	.foot .btn-plus {
-			width: 28px;
-			padding: 9px 3px;
-			position: absolute;
-			left: 0px;
-			border-left: 1px solid #d9d9d9;
-	}
+.foot {
+	right: 0px;
+	min-height: 48px;
+	position: fixed;
+	bottom: 0px;
+	left: 200px;
+	background-color: #f8f8f8;
+}
 
-	.foot .btn-plus i {
-			font-size: 2em;
-			color: #ccc;
-	}
+.foot .con {
+	position: absolute;
+	left: 20px;
+	line-height: 30px;
+	top: 5px;
+	right: 80px;
+}
 
-	.foot .btn-face {
-			width: 28px;
-			padding: 9px 3px 9px 0px;
-			position: absolute;
-			left: 35px;
-			/*border-right: 1px solid #d9d9d9;*/
-	}
+.foot .btn-plus {
+	width: 28px;
+	padding: 9px 3px;
+	position: absolute;
+	left: 0px;
+	border-left: 1px solid #d9d9d9;
+}
 
-	.foot .btn-face i {
-			font-size: 2em;
-			color: #d9d9d9;
-	}
+.foot .btn-plus i {
+	font-size: 2em;
+	color: #ccc;
+}
 
-	.foot .selbox {
-			height: 150px;
-			margin-top: 48px;
-			border-top: 1px solid #d9d9d9;
-	}
+.foot .btn-face {
+	width: 28px;
+	padding: 9px 3px 9px 0px;
+	position: absolute;
+	left: 35px;
+	/*border-right: 1px solid #d9d9d9;*/
+}
 
-	.show {
-			display: block;
-	}
+.foot .btn-face i {
+	font-size: 2em;
+	color: #d9d9d9;
+}
 
-	.hide {
-			display: none;
-	}
+.foot .selbox {
+	height: 150px;
+	margin-top: 48px;
+	border-top: 1px solid #d9d9d9;
+}
 
-	.face-box {
-			/* position: absolute; */
-			/* top: 48px; */
-			/* left: 0px; */
-			/* right: 0px; */
-			/* bottom: 0px; */
-			padding: 15px 15px 0px 15px;
-			overflow: hidden;
-			width: 290px;
-			margin: 0px auto;
-			height: 135px;
-	}
+.show {
+	display: block;
+}
 
-	.face-box li {
-			width: 30px;
-			float: left;
-			padding: 6px 10px 0px 8px;
-	}
+.hide {
+	display: none;
+}
 
-	.btn {
-			display: inline-block;
-			vertical-align: top;
-			font-size: 14px;
-			line-height: 32px;
-			margin-left: 5px;
-			padding: 0 6px;
-			background-color: #33DF83;
-			color: #fff;
-			border-radius: 3px;
-	}
+.face-box {
+	/* position: absolute; */
+	/* top: 48px; */
+	/* left: 0px; */
+	/* right: 0px; */
+	/* bottom: 0px; */
+	padding: 15px 15px 0px 15px;
+	overflow: hidden;
+	width: 290px;
+	margin: 0px auto;
+	height: 135px;
+}
 
-	.btn-send {
-		text-align: center;
-    position: absolute;
-    right: 5px;
-    width: 65px;
-    top: 7px;
-	}
+.face-box li {
+	width: 30px;
+	float: left;
+	padding: 6px 10px 0px 8px;
+}
+
+.btn {
+	display: inline-block;
+	vertical-align: top;
+	font-size: 14px;
+	line-height: 32px;
+	margin-left: 5px;
+	padding: 0 6px;
+	background-color: #33df83;
+	color: #fff;
+	border-radius: 3px;
+}
+
+.btn-send {
+	text-align: center;
+	position: absolute;
+	right: 5px;
+	width: 65px;
+	top: 7px;
+}
 </style>
